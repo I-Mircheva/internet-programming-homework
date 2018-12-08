97b53af8a93aca9fbe81ffcc1e93cfd75b18b60d
Ivona Mircheva <ivona1mircheva@gmail.com>
2018-12-08 22:48:06 +0200

    Fix RegEx ex 2 and 5

--- 4/input.js
+++ 4/input.js
@@ -1,9 +1,9 @@
 regExps = {
 "exercise_1": /([A-Z][a-z]+)/,
-"exercise_2": /(088[1-9]{7})/,
+"exercise_2": /(088[1-9]{1}[0-9]{6})/,
 "exercise_3": /([^0-1]+)/,
 "exercise_4": /(^[a-z][^\@\$]{2,13}$)/,
-"exercise_5": /((^[0-9]{3}$)|([1][0-4][0-9][0-9]$)|1500)/,
+"exercise_5": /((^[0-9]{1,3}$)|([1][0-4][0-9][0-9]$)|1500)/,
 "exercise_6": /(class=["'].*["'])/
 };
 cssSelectors = {
@@ -15,14 +15,5 @@ cssSelectors = {
 "exercise_6": "item#someId item item item item",
 "exercise_7": "css > different > different:nth-child(2) > java:nth-child(2)",
 "exercise_8": "#someId"
-
-// "exercise_1": ["3-2"],
-// "exercise_2": ["4-1-1","4-1-2","4-2-1","4-2-2"],
-// "exercise_3": ["3-2-3"],
-// "exercise_4": ["3"],
-// "exercise_5": ["1-1-2"],
-// "exercise_6": ["2-1-1-2-1", "2-1-2-1-1"],
-// "exercise_7": ["4-2-2"],
-// "exercise_8": ["2"]
 
 };
